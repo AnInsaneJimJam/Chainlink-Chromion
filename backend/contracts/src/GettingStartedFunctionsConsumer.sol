50226644525d6fe087cde63fd4e86a8dd4e40ead
12anand34 <anand47bansal#gmail.com>
2025-06-26 20:27:01 +0000

    compatability error

--- backend/contracts/src/GettingStartedFunctionsConsumer.sol
+++ backend/contracts/src/GettingStartedFunctionsConsumer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 import {FunctionsClient} from "@chainlink/contracts@1.4.0/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
 import {ConfirmedOwner} from "@chainlink/contracts@1.4.0/src/v0.8/shared/access/ConfirmedOwner.sol";
